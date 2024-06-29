b1f94ca2933992c47d1d812b283c7f6a5346a9c7
lino tom <linotom04@gmail.com>
2024-06-30 02:06:30 +0530

    AddBus Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ViewAllBus from './components/ViewAllBus';
+import AddBus from './components/AddBus';
 
 function App() {
   return (
     <div>
       <ViewAllBus/>
+      <AddBus/>
     </div>
   );
 }
