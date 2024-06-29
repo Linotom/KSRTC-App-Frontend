8a9874c1855142b61c44359847fe99a9b1163641
lino tom <linotom04@gmail.com>
2024-06-30 02:12:19 +0530

    SearchBus Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ViewAllBus from './components/ViewAllBus';
 import AddBus from './components/AddBus';
+import SearchBus from './components/SearchBus';
 
 function App() {
   return (
     <div>
       <ViewAllBus/>
       <AddBus/>
+      <SearchBus/>
     </div>
   );
 }
